b0b30e6ac14b0f14afcdbac322ea02e400d189dd
BQSGWYS <bqsgwys@outlook.com>
2016-05-04 18:54:38 +0800

    fix bug
    fix a important bug

--- router/create/index.js
+++ router/create/index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 var admin = require('./admin');
 var sites = require('./sites')
-var user = require('./user')
 
 router.use('/admin',admin);
 router.use('/sites',sites);
-router.use('/',user);
 module.exports = router;
